cbc1e5e23e7827c6ca5eae357da72c7357e159e0
Clenilson Sousa <clenilson.silva4@hotmail.com>
2024-06-27 22:42:54 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,68 @@
+let listaNumerosSorteados = [];
+let numeroMaximo = 2;
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
+
+exibirMensagemInicial();
+
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+    
+    if (chute == numeroSecreto) {
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}`;
+
+        exibirTextoNaTela('h1', 'Acertou!');
+        exibirTextoNaTela('p', mensagemTentativas);
+
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela('p', 'O número secreto é menor');
+        } else {
+            exibirTextoNaTela('p', 'O número secreto é maior');
+        }
+
+        tentativas++;
+        limparCampo();
+    }
+}
+
+function reiniciarJogo() {
+    tentativas = 1;
+    numeroSecreto = gerarNumeroAleatorio();
+
+    limparCampo();
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+}
+
+function limparCampo() {
+    chute = document.querySelector('input').value = '';
+}
+
+function exibirTextoNaTela(tag, texto) {
+    document.querySelector(tag).innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
+}
+
+function exibirMensagemInicial() {
+    exibirTextoNaTela('h1', 'Jogo do Número Secreto');
+    exibirTextoNaTela('p', `Escolha um número entre 1 e ${numeroMaximo}`.toString());
+}
+
+function gerarNumeroAleatorio() {
+    let numeroSorteado = parseInt(Math.random() * numeroMaximo + 1);
+
+    if (listaNumerosSorteados.length == numeroMaximo) {
+        listaNumerosSorteados = [];
+    }
+
+    if (listaNumerosSorteados.includes(numeroSorteado)) {
+        return gerarNumeroAleatorio();
+    } else {
+        listaNumerosSorteados.push(numeroSorteado);
+        console.log(listaNumerosSorteados);
+        return numeroSorteado;
+    }
+}
